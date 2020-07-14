9052475a8d9ab942d509c11a68020b9765f35531
jjzcru <jjzcru@gmail.com>
2020-07-14 17:30:59 -0500

    Update develop branch

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Unstock Admin</a>
+          Welcome to<a href="https://nextjs.org">Unstock Admin</a>
         </h1>
 
         <p className="description">
